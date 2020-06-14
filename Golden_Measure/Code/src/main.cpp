154f3d54e9835a668d40bbcc8ee537db09b358d8
Niceta-Nduku <nicetanduku@gmail.com>
2020-06-14 17:59:06 +0200

    Added file reading option for c++ ps

--- Golden_Measure/Code/src/main.cpp
+++ Golden_Measure/Code/src/main.cpp
@@ -1,18 +1,72 @@
 #include "Search.h"
 #include <time.h>
+#include <string.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
-int main() {
-   std::string mainString = "ABAAABCDBBABCDDEBCABC";
-   std::string pattern = "ABC";
-   int index = -1;
-   clock_t time = clock();
-   int * locArray =  YODA_PSA::naivePatternSearch(mainString.c_str(), pattern.c_str(), mainString.size(),pattern.size(),0, &index);
-   time = clock() - time;
-   for(int i = 0; i <= index; i++) {
-      std::cout << "Pattern found at position: " << locArray[i]<<std::endl;
+char *readfile(const std::string filename, const int numOfChar)
+{
+
+   unsigned char *inputstream;
+   //input stream
+   std::ifstream inFile;
+   std::string line;
+
+   inFile.open(filename, std::ifstream::binary);
+
+   if (!inFile)
+   {
+      std::cerr << "Unable to open file" << filename << std::endl;
+      inFile.close();
+      return 0;
+   }
+   else
+   {
+
+      inputstream = new unsigned char[numOfChar];
+      //add characters to the array
+      inFile.read((char *)inputstream, numOfChar);
+   }
+   inFile.close();
+
+   return (char *)inputstream;
+}
+
+int main(int argc, char *argv[])
+{
+   if (argc < 3)
+   {
+      std::cout << "Missing File" << std::endl;
+      return 0;
    }
 
-   std::cout << "search took " << ((float)time)/CLOCKS_PER_SEC << " seconds" << std::endl;
+   const std::string inputFile = argv[1];
 
-    return 0;
+   const std::string sizeOfFile = argv[2];
+
+   char* pattern = argv[3];
+
+   // std::cout << pattern << std::endl;
+   
+   char* mainString = readfile(inputFile,std::stoi(sizeOfFile));
+
+   // std::cout << mainString <<std::endl;
+
+   int index = -1;
+   clock_t time = clock();
+
+   std::cout << strlen(mainString) << " " << strlen(pattern) << std::endl;
+
+   int *locArray = YODA_PSA::naivePatternSearch(mainString, pattern, strlen(mainString), strlen(pattern), 0, &index);
+   
+   time = clock() - time;
+   for (int i = 0; i <= index; i++)
+   {
+      std::cout << "Pattern found at position: " << locArray[i] << std::endl;
+   }
+
+   std::cout << "search took " << ((float)time) / CLOCKS_PER_SEC << " seconds" << std::endl;
+
+   return 0;
 }
